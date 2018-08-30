7363adf5fca60770e5bd1cff860b18ebdfd6e26a
tianqiang <1366191116@qq.com>
2018-08-30 16:27:37 +0800

    新增项目

---
+++ tuyou-order/src/main/java/com/tuyou/tuyouorder/dao/BaseDao.java
@@ -0,0 +1,65 @@
+package com.tuyou.tuyouorder.dao;
+
+import java.util.List;
+import java.util.Map;
+
+public interface BaseDao<Entity> {
+    /**
+     * 根据ID号查询单个实体
+     * @param m
+     */
+    Entity find(Map m);
+
+    /**
+     * 根据实体对象查询
+     * @param m
+     * @return
+     */
+    List<Entity> findEntity(Map m);
+
+    /**
+     * 由分页信息查询分页记录
+     * @param m
+     * @return
+     */
+    List<Entity> findLimit(Map m);
+
+
+    /**
+     * 为分页查询出记录总数
+     * @param m
+     * @return
+     */
+    Integer findCount(Map m);
+
+    /**
+     * 保存全部字段
+     * @param m
+     * @return
+     */
+    int save(Map m);
+    /**
+     * 插入
+     * @param m
+     */
+    Integer saveNotNull(Map m);
+
+    /**
+     * 根据ID号修改单个实体
+     * @param m
+     */
+    Integer upt(Map m);
+
+    /**
+     * 根据ID号删除单个实体
+     * @param m
+     */
+    Integer del(Map m);
+
+    /**
+     * 批量插入
+     * @param m
+     * @return
+     */
+    Integer insertTranscationList(Map m);
+}
---
+++ tuyou-order/src/main/java/com/tuyou/tuyouorder/dao/UserInfoMapper.java
@@ -0,0 +1,10 @@
+package com.tuyou.tuyouorder.dao;
+
+import org.springframework.stereotype.Repository;
+
+import java.util.Map;
+
+@Repository
+public interface UserInfoMapper extends BaseDao<Map> {
+
+}
