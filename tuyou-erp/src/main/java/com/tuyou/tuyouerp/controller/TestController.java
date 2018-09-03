3fade6eca4934c08a89a4972ed34fdd1c3eafcfd
yiluobaba <476798589@qq.com>
2018-09-03 17:51:22 +0800

    修改测试类

--- tuyou-erp/src/main/java/com/tuyou/tuyouerp/controller/TestController.java
+++ tuyou-erp/src/main/java/com/tuyou/tuyouerp/controller/TestController.java
@@ -1,10 +1,13 @@
 package com.tuyou.tuyouerp.controller;
 
+import com.tuyou.common.Json.JsonUtil;
 import com.tuyou.tuyouerp.service.EnterpriseService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import com.xdjk.model.erp.Enterprise;
+
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
@@ -22,5 +25,18 @@ public class TestController extends BaseController{
     private String index(){
 
         return "Hello World!";
+    }
+    @RequestMapping("/list")
+    private JsonUtil list(JsonUtil jsonUtil){
+        Enterprise entity=(Enterprise)jsonUtil.getData();
+        if(entity==null){
+            entity=new Enterprise();
+        }
+        int count = enterpriseService.count(entity);
+        List<Enterprise> list=new ArrayList<Enterprise>();
+        if (count > 0) {
+           list = enterpriseService.search(entity, 0, 20, null);
+        }
+        return  JsonUtil.setParameter(list,count,jsonUtil);
     }
 }
