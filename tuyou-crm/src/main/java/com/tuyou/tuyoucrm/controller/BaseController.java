531ccd4901b572e914f86ffd89f506253fed5ee6
tianqiang <1366191116@qq.com>
2018-09-03 15:25:30 +0800

    删除无用文件

--- tuyou-crm/src/main/java/com/tuyou/tuyoucrm/controller/BaseController.java
+++ tuyou-crm/src/main/java/com/tuyou/tuyoucrm/controller/BaseController.java
@@ -1,7 +1,6 @@
 package com.tuyou.tuyoucrm.controller;
 
 
-import com.tuyou.common.ResponseModel;
 import org.springframework.http.HttpStatus;
 
 /**
@@ -9,14 +8,6 @@ import org.springframework.http.HttpStatus;
  */
 public abstract class BaseController {
 
-    protected ResponseModel getSuccess() {
-        ResponseModel response = new ResponseModel();
-
-        response.setMessage(HttpStatus.OK.getReasonPhrase());
-        response.setStatus(HttpStatus.OK.value());
-
-        return response;
-    }
 
 
 
